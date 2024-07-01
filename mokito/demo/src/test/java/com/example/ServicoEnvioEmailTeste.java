e90416ea34c88ce33570b4c19c20e7218ad8db60
Amanda Oliveira | Plano Angelus <amanda.oliveira@planoangelus.com.br>
2024-07-01 17:53:22 -0300

    Git commit

--- mokito/demo/src/test/java/com/example/ServicoEnvioEmailTeste.java
+++ mokito/demo/src/test/java/com/example/ServicoEnvioEmailTeste.java
@@ -1,11 +1,41 @@
 package com.example; 
 
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
-import org.junit.jupiter.api.extension.Extensions;
+import org.mockito.ArgumentCaptor;
+import org.mockito.Captor;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.junit.jupiter.MockitoExtension;
 
 @ExtendWith(MockitoExtension.class)
 public class ServicoEnvioEmailTeste {
 
     @Mock 
-       
+    private PlataformaDeEnvio plataforma;
+
+    @InjectMocks
+    private ServicoEnvioEmail servico;
+
+    @Captor
+    ArgumentCaptor<Email> captor;
+
+    @Test 
+    void validarDadosEnviadosParaAPlataforma(){
+        String enderecoDeEmail = "usuario@teste.com.br";
+        String mensagem = "Olá mundo teste mensagem";
+        boolean ehFormatoHtml = false;
+
+        servico.enviaEmail(enderecoDeEmail, mensagem, ehFormatoHtml);
+
+        Mockito.verify(plataforma).enviaEmail(captor.capture()); //Verifica na nossa plataforma qual foi o email que foi enviado
+
+        Email emailCapturado = captor.getValue();
+
+        Assertions.assertEquals(enderecoDeEmail, emailCapturado.getEnderecoEmail());
+        Assertions.assertEquals(mensagem, emailCapturado.getMensagem());
+        Assertions.assertEquals(Formato.TEXTO, emailCapturado.getFormato());
+    }
 }
